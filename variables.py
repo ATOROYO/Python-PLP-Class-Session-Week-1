e42c0ccd289649cfe5954222c6c4b015562e3432
ATOROYO <davidatoroyosika@gmail.com>
2024-10-30 15:18:42 +0300

    Assigned multiple values to variables

--- variables.py
+++ variables.py
@@ -4,3 +4,9 @@ print(site_name)
 # Assigning new value to site_name
 site_name = "I love coding 👨🏽‍💻"
 print(site_name)
+
+# Assigning multiple values to variables
+a,b,c = 5,7,"Hello World"
+print(a) # Prints 5
+print(b) # Prints 7
+print(c) # Prints Hello World
